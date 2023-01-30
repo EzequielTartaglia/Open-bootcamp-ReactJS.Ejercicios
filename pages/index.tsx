030a0ebeee0cb105e126dc8330218052c7068c3b
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2023-01-30 20:40:58 -0300

    Finish the exercise 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import CustomModal from './CustomModal'
+
+//Component to deploy the exersice 1
 import ListOfContacts from '../pages/Ejercicio Bootcamp 1 (React)/ListOfContacts'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,12 +17,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-     <CustomModal 
-     customMessage='Are you sure you wish to disconnect this game from your Quarters account'
-     onConfirm={ function() {alert('Confirmed');} }
-     />
 
+      {/* Exersice 1 */}
      <ListOfContacts/>
     </>
   )
